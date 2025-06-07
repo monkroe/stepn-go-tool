617ce82a9a39043021da6ecb90cf60a26b1ac415
Robertas <4360582+monkroe@users.noreply.github.com>
2025-06-07 15:55:21 -0500

    Delete js directory

--- js/js/supabase.js
+++
@@ -1,4 +0,0 @@
-
-const SUPABASE_URL = 'https://zojhurhwmceoqxkatvkx.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpvamh1cmh3bWNlb3F4a2F0dmt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNjYxNDYsImV4cCI6MjA2NDc0MjE0Nn0.NFGhQc7H95U9vOaM7OVxNUgTuXSughz8ZuxaCLfbfQE';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
