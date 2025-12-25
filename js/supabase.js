765a34339b5622b9d6cd03273b8fcc77dab281be
Robertas <4360582+monkroe@users.noreply.github.com>
2025-12-25 02:31:40 -0600

    Update Supabase client initialization method

--- js/supabase.js
+++ js/supabase.js
@@ -2,5 +2,6 @@
 const SUPABASE_URL = 'https://zojhurhwmceoqxkatvkx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpvamh1cmh3bWNlb3F4a2F0dmt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNjYxNDYsImV4cCI6MjA2NDc0MjE0Nn0.NFGhQc7H95U9vOaM7OVxNUgTuXSughz8ZuxaCLfbfQE';
 
-// PAKEITIMAS: Priskiriame tiesiai į window objektą, kad matytų kiti failai
+// PAKEITIMAS: Naudojame biblioteką klientui sukurti ir iškart priskiriame jį globaliam kintamajam
+// Dabar 'window.supabase' bus Jūsų prisijungęs klientas, o ne tik biblioteka.
 window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
